6009fdf1695684d450f1dc398a60cd9dda9d8bf3
Nguyễn Đức An <117847156+NguyenDucAnforwork@users.noreply.github.com>
2024-08-02 20:45:33 +0700

    Update ML_app.py

--- ML_app.py
+++ ML_app.py
@@ -5,5 +5,7 @@ st.write("Hello world")
 
 st.info("This is a ML app") 
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+with st.expander('Data'):
+  st.write('Raw data')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
