014692f7f201ed2d057a7bb7fccc41a42d2c7c4d
Nguyễn Đức An <117847156+NguyenDucAnforwork@users.noreply.github.com>
2024-08-02 23:13:49 +0700

    Update ML_app.py

--- ML_app.py
+++ ML_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 st.title("App name")
 st.write("Hello world")
