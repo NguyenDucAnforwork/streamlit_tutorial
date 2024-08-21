d151e7d3d84ce198e83a0410a0119a04f1755501
Nguyễn Đức An <117847156+NguyenDucAnforwork@users.noreply.github.com>
2024-08-22 00:57:12 +0700

    Update ML_app.py

--- ML_app.py
+++ ML_app.py
@@ -9,7 +9,7 @@ st.write("Predict CO2 Emissions based on vehicle features")
 st.info("This is a Machine Learning app for regression")
 
 # Load the saved regression model
-with open('regressor_model.pkl', 'rb') as file:
+with open('model.pkl', 'rb') as file:
     regressor = pickle.load(file)
 
 with st.expander('Data'):
